602a76a412d5de52fbb5079ec3567cb001c98085
Keymanzx <jae_39@hotmail.com>
2022-01-18 09:40:21 +0700

    update 114
    update 114

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ const path = require('path');
 
 const res = require('express/lib/response');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
 app.get("/",(req,res) =>{
 
-    res.send('Hello Node JS Express .....  ');
+    res.render('index',{username: 'User1', customers: ["dev","dev2","dev3"]});
 
 })
 
-app.listen(port,() =>{
+app.listen(PORT,() =>{
 
-    debug("Listening on port", chalk.green(" : ",port));
+    debug("Listening on port", chalk.green(" : ",PORT));
 
 })
